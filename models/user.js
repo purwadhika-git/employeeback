0701ba1f270f5dead0e41c98e440ba0edb1622f5
HASBI\ACER <hasbifadillah@outlook.com>
2018-01-15 04:15:54 +0700

    Second Commit

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/employeemanagement", { useMongoClient : true });
+//mongoose.connect("mongodb://127.0.0.1:27017/employeemanagement", { useMongoClient : true });
+mongoose.connect("mongodb://admin:abc123@ds255797.mlab.com:55797/heroku_0rc9hrwf", { useMongoClient : true });
 
 const Schema = mongoose.Schema;
 
